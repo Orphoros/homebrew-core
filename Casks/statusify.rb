bba1a03cc8ff60778dc630103e42bf641177f78d
Orphoros <orp@orphoros.dev>
2024-05-18 17:30:02 +0200

    chore(statusify): update to v0.0.11

--- Casks/statusify.rb
+++ Casks/statusify.rb
@@ -1,8 +1,8 @@
 cask "statusify" do
-  version "0.0.10"
-  arch arm: "x86_64", intel: "x86_64"
-  sha256 arm:   "3e7979212cd9705119118d1c04c4b4eae9270b141c97054ba087a1dc78df5c76",
-         intel: "3e7979212cd9705119118d1c04c4b4eae9270b141c97054ba087a1dc78df5c76"
+  version "0.0.11"
+  arch arm: "aarch64", intel: "x86_64"
+  sha256 arm:   "99be8149f6e9cc7913e14d7239e5f0ff9bf4ce1e59f355863489818087d67cd5",
+         intel: "820197bedfb4ca0c76c87acea177aac3d84b1c026092733ac409a0e62012081c"
 
   url "https://github.com/Orphoros/Statusify/releases/download/v#{version}/Statusify.darwin-#{arch}.dmg"
   name "statusify"
