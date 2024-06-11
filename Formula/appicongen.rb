abb87c35cdbf0258aebcc83812182ed502c063f0
Orphoros <orp@orphoros.dev>
2024-06-11 17:17:25 +0200

    fix(appicongen): correct checksum

--- Formula/appicongen.rb
+++ Formula/appicongen.rb
@@ -4,12 +4,12 @@ class Appicongen < Formula
   homepage "https://github.com/Orphoros/AppIconGen"
   license "GPL-3.0"
 
-  if Hardware::CPU.intel?
-    url "https://github.com/Orphoros/AppIconGen/releases/download/v#{version}/appicongen.x86_64-apple-darwin.tar.gz"
-    sha256 "67b0be644ed82b4b4cb4476ed67d5075dd3683450d3368b2c6f9ac23f3e5bf7e"
-  elsif Hardware::CPU.arm?
+  if Hardware::CPU.arm?
     url "https://github.com/Orphoros/AppIconGen/releases/download/v#{version}/appicongen.aarch64-apple-darwin.tar.gz"
-    sha256 "741d3fde8d4f02647060672104d90a5a7ff9445aa11450e4e9264717630889c8"
+    sha256 "183a53afdd843668eed6137f50dc87b819b782d094030f3005bbede52496d0ad"
+  elsif Hardware::CPU.intel?
+    url "https://github.com/Orphoros/AppIconGen/releases/download/v#{version}/appicongen.x86_64-apple-darwin.tar.gz"
+    sha256 "8960239e2d9d78be1b9a6c604891e08791ec45afba21c7384e5823a8716c58cb"
   end
 
   livecheck do
