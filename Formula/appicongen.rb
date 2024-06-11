73a6fb57ed929e561561ea4c86948f13a98d8d11
Orphoros <orp@orphoros.dev>
2024-06-11 17:06:05 +0200

    fix(appicongen): define arch with if-block

--- Formula/appicongen.rb
+++ Formula/appicongen.rb
@@ -1,12 +1,16 @@
 class Appicongen < Formula
   version "0.0.1"
-  arch arm: "aarch64", intel: "x86_64"
-  sha256 arm:   "741d3fde8d4f02647060672104d90a5a7ff9445aa11450e4e9264717630889c8",
-         intel: "67b0be644ed82b4b4cb4476ed67d5075dd3683450d3368b2c6f9ac23f3e5bf7e"
-  url "https://github.com/Orphoros/AppIconGen/releases/download/v#{version}/appicongen.#{arch}-apple-darwin.tar.gz"
   desc "Utility that creates ICNS and ICO files from a PNG file"
   homepage "https://github.com/Orphoros/AppIconGen"
   license "GPL-3.0"
+
+  if Hardware::CPU.intel?
+    url "https://github.com/Orphoros/AppIconGen/releases/download/v#{version}/appicongen.x86_64-apple-darwin.tar.gz"
+    sha256 "67b0be644ed82b4b4cb4476ed67d5075dd3683450d3368b2c6f9ac23f3e5bf7e"
+  elsif Hardware::CPU.arm?
+    url "https://github.com/Orphoros/AppIconGen/releases/download/v#{version}/appicongen.aarch64-apple-darwin.tar.gz"
+    sha256 "741d3fde8d4f02647060672104d90a5a7ff9445aa11450e4e9264717630889c8"
+  end
 
   livecheck do
     url :url
@@ -15,5 +19,9 @@ class Appicongen < Formula
 
   def install
     bin.install "appicongen"
+  end
+
+  test do
+    system "#{bin}/your-binary-name", "--version"
   end
 end
