a44b3549babc7f2f49a5263eb4a2c19cde6d96f7
Orphoros <orp@orphoros.dev>
2024-07-07 17:41:18 +0200

    chore(appicongen): update to v1.0.1

--- Formula/appicongen.rb
+++ Formula/appicongen.rb
@@ -1,15 +1,15 @@
 class Appicongen < Formula
-  version "1.0.0"
+  version "1.0.1"
   desc "Utility that creates ICNS and ICO files from a PNG file"
   homepage "https://github.com/Orphoros/AppIconGen"
   license "GPL-3.0"
 
   if Hardware::CPU.arm?
     url "https://github.com/Orphoros/AppIconGen/releases/download/v#{version}/appicongen.aarch64-apple-darwin.tar.gz"
-    sha256 "c027ef1b68e5c523e90cfc6e30cd775375c05de31d7760568aa63e80a122158a"
+    sha256 "88c2cdad119609e033e4a82e1545c9be85d5f3d1d2e258ede02bd4f917d4f017"
   elsif Hardware::CPU.intel?
     url "https://github.com/Orphoros/AppIconGen/releases/download/v#{version}/appicongen.x86_64-apple-darwin.tar.gz"
-    sha256 "a28a2a0f5a440df5cd5997b24382f275e37247e38e63d92a138ff33c89656631"
+    sha256 "b4a187d8f6d1d6d39a27bd62a32f4a0e4ee47e602215bef8f3865f7ae779f3b7"
   else
     odie "Your architecture is not supported by this formula."
   end
